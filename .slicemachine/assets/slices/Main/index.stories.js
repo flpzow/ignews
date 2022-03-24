bce6eba6decbaf815c5f0de793b47e7f9a4a0cd8
Felipe de Castro <castrofe55@gmail.com>
2022-03-24 01:26:07 -0300

    missing some tweaks to finish

---
+++ .slicemachine/assets/slices/Main/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Main';
+
+export default {
+  title: 'slices/Main'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"main","items":[],"primary":{"title":[{"type":"heading1","text":"Cultivate impactful niches","spans":[]}],"description":[{"type":"paragraph","text":"Sunt exercitation pariatur adipisicing cupidatat minim.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Innovate best-of-breed bandwidth","spans":[]}],"description":[{"type":"paragraph","text":"Sint in do esse sint et officia ut.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
